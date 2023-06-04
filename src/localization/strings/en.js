993113b50d8f0e3733813be87c48c99c16e5aee1
Oleh Zankiv <support@devage.tech>
2023-06-04 13:29:52 +0300

    Added text component

--- src/localization/strings/en.js
+++ src/localization/strings/en.js
@@ -1,3 +1,4 @@
 export const en = {
   "Sign Up": "Sign Up",
+  "GooseTrack's Calendar view provides a comprehensive overview of your schedule, displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.": "GooseTrack's Calendar view provides a comprehensive overview of your schedule, displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.",
 }
--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -7,11 +7,14 @@ import {
   getTabletStyles,
   useBreakpointValue,
 } from '../../styles/breakpoints'
-import { Modal } from '../../components'
+import { Modal, Text } from '../../components'
 import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 
 const LoginPage = ({}) => {
-  const [visible, setVisible] = useState(false)
+  const { t } = useTranslation()
+
+  const [visible, setVisible] = useState(true)
 
   const value = useBreakpointValue({
     desktopValue: 'Welcome to Desktop',
@@ -28,7 +31,11 @@ const LoginPage = ({}) => {
         onClose={() => setVisible(false)}
         onEnterPress={() => setVisible(false)}
       >
-        <p>Create a task</p>
+        <Text type='p'>
+          {t(
+            "GooseTrack's Calendar view provides a comprehensive overview of your schedule, displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.",
+          )}
+        </Text>
       </Modal>
 
       <button onClick={() => setVisible(true)}>Open</button>
