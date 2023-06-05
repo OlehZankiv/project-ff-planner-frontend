dc43e42c30491a1cf6851fdc79a041439038fa09
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-06-05 19:18:55 +0300

    Add phrases from main layout and sidebar to localization (en)

--- src/localization/strings/en.js
+++ src/localization/strings/en.js
@@ -1,4 +1,11 @@
 export const en = {
   "Sign Up": "Sign Up",
+  "User Pane": "User Pane",
+  "My Account": "My Account",
+  "Calendar": "Calendar",
+  "Log out": "Log out",
+  "Feedback": "Feedback",
+  "User Profile": "User Profile",
+  "Let go of the past and focus on the present!": "Let go of the past and focus on the present!",
   "GooseTrack's Calendar view provides a comprehensive overview of your schedule, displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.": "GooseTrack's Calendar view provides a comprehensive overview of your schedule, displaying all your tasks, events, and appointments in a visually appealing and intuitive layout.",
 }
