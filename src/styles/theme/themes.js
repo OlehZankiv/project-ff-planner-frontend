6f9381ed4cbf1c2cb4d31dd69ddcccb34616dee7
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-06-06 00:07:29 +0300

    Add colors

--- src/styles/theme/themes.js
+++ src/styles/theme/themes.js
@@ -5,12 +5,15 @@ const themeCreator = ({ colors, shadows }) => ({
 
 const shadows = {
   modalShadow: '0px 4px 16px rgba(17, 17, 17, 0.1)',
+  btnShadow: '4px 2px 16px rgba(136, 165, 191, 0.48)',
 }
 
 export const lightTheme = themeCreator({
   colors: {
     background: '#ffffff',
     text: '#111111',
+    primary: '#3e85f3',
+    primaryBtnHover: '#2b78ef',
 
     modalBackground: '#ffffff',
     modalOverlay: '#13151A',
@@ -22,6 +25,8 @@ export const darkTheme = themeCreator({
   colors: {
     background: '#13151A',
     text: '#ffffff',
+    primary: '#3e85f3',
+    primaryBtnHover: '#2b78ef',
 
     modalBackground: '#171820',
     modalOverlay: '#3f3f3f',
