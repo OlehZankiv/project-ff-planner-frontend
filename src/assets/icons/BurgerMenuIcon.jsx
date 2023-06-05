033537250dd5f079ea5c7e7cb9d7e5c55d734295
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-06-05 19:17:51 +0300

    Add icons to main layout and sidebar

--- src/assets/icons/BurgerMenuIcon.jsx
+++ src/assets/icons/BurgerMenuIcon.jsx
@@ -1,20 +1,21 @@
-export const CloseIcon = ({ size, color }) => (
+export const BurgerMenuIcon = ({ size, color }) => (
   <svg
     width={size}
     height={size}
-    viewBox='0 0 33 33'
+    viewBox='0 0 32 32'
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
   >
     <path
-      d='M18 6.5L6 18.5'
+      d='M3 12H21'
       stroke={color}
       strokeWidth='2'
       strokeLinecap='round'
       strokeLinejoin='round'
     />
+    <path d='M3 6H21' stroke={color} strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
     <path
-      d='M6 6.5L18 18.5'
+      d='M3 18H21'
       stroke={color}
       strokeWidth='2'
       strokeLinecap='round'
--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,2 +1,8 @@
 export { LoginIcon } from './LogInIcon'
 export { CloseIcon } from './CloseIcon'
+export { UserCheckIcon } from './UserCheckIcon'
+export { CalendarCheckOutIcon } from './CalendarCheckoutIcon'
+export { BurgerMenuIcon } from './BurgerMenuIcon'
+export { MoonIcon } from './MoonIcon'
+export { SunIcon } from './SunIcon'
+export { LogOutIcon } from './LogOutIcon'
