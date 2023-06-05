19abe7b0d6e7f9dd07fa82954faa7b8a64bc9806
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-06-06 00:07:44 +0300

    Update MainLayout.jsx

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,39 +1,116 @@
 import { Suspense, useState } from 'react'
 import { Outlet } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
+import { useTranslation } from 'react-i18next'
 import { BurgerMenuIcon, MoonIcon, SunIcon } from '../../assets/icons'
 import { useAppThemeContext } from '../../styles/theme/provider'
-import { useDimensions } from '../../hooks'
 import { SideBar } from '../SideBar'
+import {
+  useBreakpointValue,
+  ScreenWrapper,
+  getBreakpointsStyles,
+  getDesktopStyles,
+} from '../../styles/breakpoints'
+import { PrimaryColorBtn } from '../PrimaryColorBtn'
+import { Text } from '../Text'
 
 export const MainLayout = () => {
+  const { t } = useTranslation()
   const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false)
-  const { width } = useDimensions()
+  const [selectedTab, setSelectedTab] = useState('calendar')
   const { themeType, setThemeType } = useAppThemeContext()
+  const iconSize = useBreakpointValue({ mobileValue: 24, tabletValue: 32, desktopValue: 32 })
+  const nameFontSize = useBreakpointValue({
+    mobileValue: 14,
+    tabletValue: 18,
+    desktopValue: 18,
+  })
+
+  const userName = 'Yuliia'
 
   const handleThemeChange = () => {
     setThemeType(themeType === 'light' ? 'dark' : 'light')
   }
+
+  const firstLetersMaker = (str) => {
+    const words = str.split(' ')
+    const capitalizedWords = words
+      .map((word) => {
+        return word.split('')[0].toUpperCase()
+      })
+      .join('')
+    return capitalizedWords
+  }
+
   return (
-    <MainWrap>
-      {width > 1024 && (
+    <MainWrap theme={themeType}>
+      {isBurgerMenuOpen && (
         <SideBar
           isBurgerMenuOpen={isBurgerMenuOpen}
           setIsBurgerMenuOpen={setIsBurgerMenuOpen}
           theme={themeType}
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
         />
       )}
+      <ScreenWrapper
+        desktop={
+          <SideBar
+            isBurgerMenuOpen={isBurgerMenuOpen}
+            setIsBurgerMenuOpen={setIsBurgerMenuOpen}
+            theme={themeType}
+          />
+        }
+      />
       <ContentWrap>
         <Header>
           <Container>
             <HeaderWrap>
-              {width < 1024 && <BurgerMenuIcon size={24} />}
+              <BurgerWrap>
+                <ButtonWrap onClick={() => setIsBurgerMenuOpen(true)}>
+                  <BurgerMenuIcon
+                    size={iconSize}
+                    color={themeType === 'light' ? ' rgba(52, 52, 52, 1)' : '#fff'}
+                  />{' '}
+                </ButtonWrap>
+              </BurgerWrap>
+              <DesctopTitleWrap>
+                <Text
+                  type={'h1'}
+                  fontSize={32}
+                  fontWeight={700}
+                  lineHeight={1}
+                  color={themeType === 'light' ? '#111111' : '#fff'}
+                  style={{
+                    textShadow:
+                      '0px 47px 355px rgba(0, 0, 0, 0.07),0px 9.4px 57.6875px rgba(0, 0, 0, 0.035)',
+                  }}
+                >
+                  {selectedTab === 'calendar' ? 'Calendar' : 'User Profile'}
+                </Text>
+              </DesctopTitleWrap>
               <TabWrap>
-                <button onClick={handleThemeChange}>
-                  {themeType === 'light' ? <MoonIcon size={24} /> : <SunIcon size={24} />}
-                </button>
-                <UserName>Nadia</UserName>
+                {selectedTab === 'calendar' && <PrimaryColorBtn>{t('Feedback')}</PrimaryColorBtn>}
+                <InfoWrap>
+                  <ButtonWrap onClick={handleThemeChange}>
+                    {themeType === 'light' ? (
+                      <MoonIcon size={iconSize} />
+                    ) : (
+                      <SunIcon size={iconSize} />
+                    )}
+                  </ButtonWrap>
+                  <Text
+                    type={'p'}
+                    color={themeType === 'light' ? ' #34343' : '#fff'}
+                    fontWeight={700}
+                    lineHeight={1.29}
+                    fontSize={nameFontSize}
+                  >
+                    {userName}
+                  </Text>
+                  <AvatarWrap>{firstLetersMaker(userName)}</AvatarWrap>
+                </InfoWrap>
               </TabWrap>
             </HeaderWrap>
           </Container>
@@ -49,7 +126,8 @@ export const MainLayout = () => {
 const MainWrap = styled.div`
   width: 100%;
   min-height: 100vh;
-  background-color: #f7f6f9;
+  background-color: ${(props) =>
+    props.theme === 'light' ? 'rgba(247, 246, 249, 1)' : 'rgba(23, 24, 32, 1)'};
   display: flex;
 `
 
@@ -70,10 +148,10 @@ const Header = styled.header`
 
 const Container = styled.div`
   width: 100%;
-  max-width: 375px;
 `
 
 const HeaderWrap = styled.div`
+  width: 100%;
   display: flex;
   justify-content: space-between;
 `
@@ -81,14 +159,55 @@ const HeaderWrap = styled.div`
 const TabWrap = styled.div`
   display: flex;
   align-items: center;
+  gap: 24px;
+`
+
+const InfoWrap = styled.div`
+  display: flex;
+  align-items: center;
   gap: 8px;
 `
 
-const UserName = styled.p`
-  font-family: 'Inter';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 14px;
-  line-height: 1.29;
-  color: #343434;
+const ButtonWrap = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+`
+
+const BurgerWrap = styled.div`
+  ${getDesktopStyles(css`
+    display: none;
+  `)}
+`
+
+const AvatarWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 32px;
+  height: 32px;
+  border: 1.8px solid #3e85f3;
+  border-radius: 50%;
+
+  ${getBreakpointsStyles({
+    tablet: css`
+      width: 44px;
+      height: 44px;
+    `,
+    desktop: css`
+      width: 44px;
+      height: 44px;
+    `,
+  })}
+`
+
+const DesctopTitleWrap = styled.div`
+  display: none;
+  ${getDesktopStyles(css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `)}
 `
