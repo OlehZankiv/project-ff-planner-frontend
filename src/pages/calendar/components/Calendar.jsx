85e3207b8a46054aa1f44da0207af0d9af27d78d
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2023-06-06 20:38:31 +0300

    Adds calendar

--- src/pages/calendar/components/Calendar.jsx
+++ src/pages/calendar/components/Calendar.jsx
@@ -3,8 +3,9 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import styled from 'styled-components'
 
-export const Calendar = ({ selectedDate, setSelectedDate }) => {
+export const Calendar = ({ selectedDate, setSelectedDate }) => { // добавить пропс setSelectedDate
   console.log(selectedDate)
+  
   return (
     <CalendarWrapper>
       <FullCalendar
@@ -29,10 +30,12 @@ const CalendarWrapper = styled.section`
     max-height: 625px;
     background: #ffffff;
     border-radius: 8px;
-    cursor: pointer; // добавить курсор на день
   }
   & .fc-theme-standard td {
     border: 1px solid rgba(220, 227, 229, 0.8);
+  }
+  & .fc-daygrid-day {
+cursor: pointer;
   }
   & .fc-day-today.fc-daygrid-day {
     background-color: #fff;
@@ -51,18 +54,13 @@ const CalendarWrapper = styled.section`
     margin-top: 14px;
     margin-right: 14px;
     padding: 0;
-    /* background-color: red; */
       display: flex;
       justify-content: center;
       align-items: center;
       width: 26px;
       height: 26px;
-    font-family: 'Inter';
-    font-style: normal;
     font-weight: 700;
     font-size: 16px;
-    line-height: 1, 12px;
-    text-transform: uppercase;
     color: #343434;
   }
   & .fc-day-disabled {
