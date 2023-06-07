d0f5dfc2c9c5c9ff7e6f23be6e8d61b5215f97f8
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2023-06-07 18:20:38 +0300

    Adds code update

--- src/pages/calendar/components/CalendarWeek.jsx
+++ src/pages/calendar/components/CalendarWeek.jsx
@@ -6,67 +6,109 @@ export const CalendarWeek = () => {
   const { setThemeType } = useAppThemeContext()
   setThemeType('dark')
 
-  const mon = useBreakpointValue({
-    desktopValue: 'mon',
-    tabletValue: 'mon',
-    mobileValue: 'm',
-  })
-  const tue = useBreakpointValue({
-    desktopValue: 'tue',
-    tabletValue: 'tue',
-    mobileValue: 't',
-  })
-  const wed = useBreakpointValue({
-    desktopValue: 'wed',
-    tabletValue: 'wed',
-    mobileValue: 'w',
-  })
-  const thu = useBreakpointValue({
-    desktopValue: 'thu',
-    tabletValue: 'thu',
-    mobileValue: 't',
-  })
-  const fri = useBreakpointValue({
-    desktopValue: 'fri',
-    tabletValue: 'fri',
-    mobileValue: 'f',
-  })
-  const sut = useBreakpointValue({
-    desktopValue: 'sut',
-    tabletValue: 'sut',
-    mobileValue: 's',
-  })
-  const sun = useBreakpointValue({
-    desktopValue: 'sun',
-    tabletValue: 'sun',
-    mobileValue: 's',
-  })
+  // const mon = useBreakpointValue({
+  //   desktopValue: 'mon',
+  //   tabletValue: 'mon',
+  //   mobileValue: 'm',
+  // })
+  // const tue = useBreakpointValue({
+  //   desktopValue: 'tue',
+  //   tabletValue: 'tue',
+  //   mobileValue: 't',
+  // })
+  // const wed = useBreakpointValue({
+  //   desktopValue: 'wed',
+  //   tabletValue: 'wed',
+  //   mobileValue: 'w',
+  // })
+  // const thu = useBreakpointValue({
+  //   desktopValue: 'thu',
+  //   tabletValue: 'thu',
+  //   mobileValue: 't',
+  // })
+  // const fri = useBreakpointValue({
+  //   desktopValue: 'fri',
+  //   tabletValue: 'fri',
+  //   mobileValue: 'f',
+  // })
+  // const sut = useBreakpointValue({
+  //   desktopValue: 'sut',
+  //   tabletValue: 'sut',
+  //   mobileValue: 's',
+  // })
+  // const sun = useBreakpointValue({
+  //   desktopValue: 'sun',
+  //   tabletValue: 'sun',
+  //   mobileValue: 's',
+  // })
+
+  const days = [
+    {
+      id: '1',
+      value: useBreakpointValue({ desktopValue: 'mon', tabletValue: 'mon', mobileValue: 'm' }),
+    },
+    {
+      id: '2',
+      value: useBreakpointValue({ desktopValue: 'tue', tabletValue: 'tue', mobileValue: 't' }),
+    },
+    {
+      id: '3',
+      value: useBreakpointValue({ desktopValue: 'wed', tabletValue: 'wed', mobileValue: 'w' }),
+    },
+    {
+      id: '4',
+      value: useBreakpointValue({ desktopValue: 'thu', tabletValue: 'thu', mobileValue: 't' }),
+    },
+    {
+      id: '5',
+      value: useBreakpointValue({ desktopValue: 'fri', tabletValue: 'fri', mobileValue: 'f' }),
+    },
+    {
+      id: '6',
+      value: useBreakpointValue({ desktopValue: 'sut', tabletValue: 'sut', mobileValue: 's' }),
+    },
+    {
+      id: '7',
+      value: useBreakpointValue({ desktopValue: 'sun', tabletValue: 'sun', mobileValue: 's' }),
+    },
+  ]
+
   return (
     <WeekWrapper>
       <WeekList>
-        <WeekItem>
-          <WeekDay>{mon}</WeekDay>
-        </WeekItem>
-        <WeekItem>
-          <WeekDay>{tue}</WeekDay>
-        </WeekItem>
-        <WeekItem>
-          <WeekDay>{wed}</WeekDay>
-        </WeekItem>
-        <WeekItem>
-          <WeekDay>{thu}</WeekDay>
-        </WeekItem>
-        <WeekItem>
-          <WeekDay>{fri}</WeekDay>
-        </WeekItem>
-        <WeekItem>
-          <WeekDay>{sut}</WeekDay>
-        </WeekItem>
-        <WeekItem>
-          <WeekDay>{sun}</WeekDay>
-        </WeekItem>
+        {days.map(({ id, value }) => (
+          <WeekItem key={id}>
+            <WeekDay>{value}</WeekDay>
+          </WeekItem>
+        ))}
       </WeekList>
     </WeekWrapper>
+    
+    // <WeekWrapper>
+    //   <WeekList>
+    //     <WeekItem>
+    //       <WeekDay>{mon}</WeekDay>
+    //     </WeekItem>
+    //     <WeekItem>
+    //       <WeekDay>{tue}</WeekDay>
+    //     </WeekItem>
+    //     <WeekItem>
+    //       <WeekDay>{wed}</WeekDay>
+    //     </WeekItem>
+    //     <WeekItem>
+    //       <WeekDay>{thu}</WeekDay>
+    //     </WeekItem>
+    //     <WeekItem>
+    //       <WeekDay>{fri}</WeekDay>
+    //     </WeekItem>
+    //     <WeekItem>
+    //       <WeekDay>{sut}</WeekDay>
+    //     </WeekItem>
+    //     <WeekItem>
+    //       <WeekDay>{sun}</WeekDay>
+    //     </WeekItem>
+    //   </WeekList>
+    // </WeekWrapper>
   )
 }
 
@@ -99,12 +141,16 @@ const WeekList = styled.ul`
     list-style: none;
     border: ${colors.calendarBorderDefault};
     border-radius: 8px;
-                background: ${colors.content}; !important
+    background: ${colors.content}; !important
     `}
 `
 
 const WeekItem = styled.li`
   ${({ theme: { colors } }) => css`
+    cursor: pointer;
+    :hover {
+      opacity: 0.85;
+    }
     &:nth-last-child(-n + 2) div {
       color: ${colors.primary};
     }
@@ -127,7 +173,6 @@ const WeekItem = styled.li`
 
 const WeekDay = styled.div`
   ${({ theme: { colors } }) => css`
-    cursor: pointer;
     font-weight: 600;
     font-size: 16px;
     line-height: 18px;
