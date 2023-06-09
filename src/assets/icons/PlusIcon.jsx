ad2f883073d0cabb669b14c0c4d792f40d0b1475
Oleh Zankiv <support@devage.tech>
2023-06-09 21:00:58 +0300

    updated icons

--- src/assets/icons/PlusIcon.jsx
+++ src/assets/icons/PlusIcon.jsx
@@ -1,6 +1,17 @@
-export const PlusIcon = ({ color, size = 18 }) => (
-
-<svg width={size} height={size} viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M10.9997 7.33334V14.6667M7.33301 11H14.6663M20.1663 11C20.1663 16.0626 16.0623 20.1667 10.9997 20.1667C5.93706 20.1667 1.83301 16.0626 1.83301 11C1.83301 5.9374 5.93706 1.83334 10.9997 1.83334C16.0623 1.83334 20.1663 5.9374 20.1663 11Z" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    </svg>
+export const PlusIcon = ({ color, size = 24 }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox='0 0 24 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M12 5V19M5 12H19'
+      stroke={color}
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
 )
--- src/assets/icons/TrashIcon.jsx
+++ src/assets/icons/TrashIcon.jsx
@@ -1,7 +1,31 @@
-export const TrashIcon = ({ color, size = 14 }) => (
-    <svg width={size} height={size} viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M5.25 1.75H8.75" stroke={color} strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M1.75 3.5H12.25" stroke={color} strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-        <path d="M2.91699 3.5L3.26751 8.75775C3.31065 9.40488 3.33222 9.72844 3.40201 9.99909C3.69263 11.1261 4.60892 11.9834 5.75279 12.1984C6.02748 12.25 6.35176 12.25 7.00033 12.25V12.25C7.64889 12.25 7.97317 12.25 8.24786 12.1984C9.39174 11.9834 10.308 11.1261 10.5986 9.99909C10.6684 9.72844 10.69 9.40488 10.7331 8.75775L11.0837 3.5" stroke="#111111" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
+export const TrashIcon = ({ color, size = 16 }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox='0 0 16 16'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <path
+      d='M6 2H10'
+      stroke={color}
+      strokeWidth='1.3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M2 4H14'
+      stroke={color}
+      strokeWidth='1.3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M3.33334 4L3.73393 10.0089C3.80831 11.1245 3.8455 11.6823 4.0278 12.1304C4.35879 12.9441 5.02973 13.5718 5.86358 13.8479C6.32286 14 6.88191 14 8.00001 14V14C9.11811 14 9.67716 14 10.1364 13.8479C10.9703 13.5718 11.6412 12.9441 11.9722 12.1304C12.1545 11.6823 12.1917 11.1245 12.2661 10.0089L12.6667 4'
+      stroke={color}
+      strokeWidth='1.3'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
 )
