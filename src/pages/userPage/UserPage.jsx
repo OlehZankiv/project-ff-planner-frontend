7760e64dff7d1d5536eb28be5f521a369d3f4cd6
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-06-10 23:05:16 +0300

    Update UserPage.jsx

--- src/pages/userPage/UserPage.jsx
+++ src/pages/userPage/UserPage.jsx
@@ -7,7 +7,6 @@ import { Text } from '../../components/Text'
 import styled, { css } from 'styled-components'
 import { Input } from '../../components/fields/Input'
 import Avatar from '../../components/avatar/Avatar'
-import InputStatus from '../../components/InputStatus'
 import { getBreakpointsStyles, useBreakpointValue } from '../../styles/breakpoints'
 import { Button, DatePicker } from '../../components'
 import { t } from 'i18next'
@@ -63,22 +62,10 @@ const UserPage = () => {
   return (
     <UserWrapper>
       <Avatar type='profile' plus={true} styles={styles} onPlusHandler={onPlusHandler}></Avatar>
-      <Text
-        type='p'
-        color='userNameText'
-        fontWeight={1000}
-        fontSize={nameFontSize}
-        style={{ textAlign: 'center', marginBottom: 8 }}
-      >
+      <Text type='h2' color='userNameText' fontWeight={1000} fontSize={nameFontSize}>
         {userName}
       </Text>
-      <Text
-        type='p'
-        color='userAvatar'
-        fontWeight={600}
-        fontSize={userFontSize}
-        style={{ textAlign: 'center' }}
-      >
+      <Text type='p' color='userAvatar' fontWeight={600} fontSize={userFontSize}>
         User
       </Text>
       <Formik
@@ -98,79 +85,63 @@ const UserPage = () => {
           return (
             <UserForm onSubmit={handleSubmit}>
               <DivForm>
-                <InputWrapper>
-                  <Input
-                    id='username'
-                    name='username'
-                    type='text'
-                    title={'User name'}
-                    rightIcon={null}
-                    isError={errors.username && touched.username}
-                    successMessage={'This is Correct name'}
-                    placeholder={t('Enter your name')}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  ></Input>
-                  <InputStatus error={errors.username} touched={touched.username} />
-                </InputWrapper>
-
-                <InputWrapper>
-                  <DatePicker
-                    id='birthday'
-                    name='birthday'
-                    placeholder={t('Enter your birthday')}
-                    title={'Birthday'}
-                    value={values.birthday}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <InputStatus error={errors.birthday} touched={touched.birthday} />
-                </InputWrapper>
-                <InputWrapper>
-                  <Input
-                    id='email'
-                    name='email'
-                    type='text'
-                    placeholder={t('Enter your email')}
-                    title={'Email'}
-                    rightIcon={null}
-                    isError={errors.email && touched.email}
-                    successMessage={'This is Correct email'}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <InputStatus error={errors.email} touched={touched.email} />
-                </InputWrapper>
-                <InputWrapper>
-                  <Input
-                    id='phone'
-                    name='phone'
-                    type='tel'
-                    placeholder={t('Enter your phone number')}
-                    title={'Phone'}
-                    rightIcon={null}
-                    isError={errors.phone && touched.phone}
-                    successMessage={'This is Correct phone'}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <InputStatus error={errors.phone} touched={touched.phone} />
-                </InputWrapper>
-                <InputWrapper>
-                  <Input
-                    id='skype'
-                    name='skype'
-                    type='text'
-                    placeholder={t('Enter your skype')}
-                    title={'Skype'}
-                    rightIcon={null}
-                    isError={errors.skype && touched.skype}
-                    successMessage={'This is Correct Skype'}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <InputStatus error={errors.skype} touched={touched.skype} />
-                </InputWrapper>
+                <Input
+                  id='username'
+                  name='username'
+                  type='text'
+                  title={'User name'}
+                  rightIcon={null}
+                  isError={errors.username && touched.username}
+                  successMessage={'This is Correct name'}
+                  placeholder={t('Enter your name')}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                ></Input>
+                <DatePicker
+                  id='birthday'
+                  name='birthday'
+                  placeholder={t('Enter your birthday')}
+                  title={'Birthday'}
+                  value={values.birthday}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <Input
+                  id='email'
+                  name='email'
+                  type='text'
+                  placeholder={t('Enter your email')}
+                  title={'Email'}
+                  rightIcon={null}
+                  isError={errors.email && touched.email}
+                  successMessage={'This is Correct email'}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <Input
+                  id='phone'
+                  name='phone'
+                  type='tel'
+                  placeholder={t('Enter your phone number')}
+                  title={'Phone'}
+                  rightIcon={null}
+                  isError={errors.phone && touched.phone}
+                  successMessage={'This is Correct phone'}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <Input
+                  id='skype'
+                  name='skype'
+                  type='text'
+                  placeholder={t('Enter your skype')}
+                  title={'Skype'}
+                  rightIcon={null}
+                  isError={errors.skype && touched.skype}
+                  successMessage={'This is Correct Skype'}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
               </DivForm>
               <Button
                 type='submit'
@@ -207,10 +178,9 @@ const styles = {
 
 export const UserWrapper = styled.div`
   ${({ theme: { colors } }) => css`
-    min-height: 474px;
-    /* margin-top: 151px; */
     background-color: ${colors.content};
-
+    display: flex;
+    flex-direction: column;
     justify-content: center;
     border-radius: 16px;
     ${getBreakpointsStyles({
@@ -220,65 +190,27 @@ export const UserWrapper = styled.div`
   `}
 `
 
-export const UserText = styled.p`
-  ${({ theme: { colors } }) => css`
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 12px;
-    line-height: 1.17;
-    margin-top: 5px;
-    color: ${colors.inputLabel};
-    text-align: center;
-    margin-bottom: 40px;
-    ${getBreakpointsStyles({
-      tablet: css`
-        font-size: 14px;
-        line-height: 1.29;
-      `,
-      desktop: css``,
-    })}
-  `}
-`
-
 export const UserForm = styled.form`
-  // width: calc(100% - 36px);
   padding: 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
-
+  justify-content: center;
   margin: 0 auto;
-  ${getBreakpointsStyles({
-    tablet: css`
-      // width: 374px;
-      // padding: 20px 175px;
-    `,
-    desktop: css`
-      // padding: 20px 164px;
-      // width: 768px;
-
-      flex-wrap: wrap;
-    `,
-  })}
 `
 export const DivForm = styled.div`
   margin-bottom: 10px;
-  width: 70%;
+  width: 100%;
   ${getBreakpointsStyles({
-    desktop: css`
-      column-count: 2;
-      column-gap: 50px;
-      flex: 1;
-      break-inside: avoid-column;
+    tablet: css`
+      width: 70%;
     `,
-  })}
-`
-export const InputWrapper = styled.div`
-  ${getBreakpointsStyles({
     desktop: css`
-      height: 120px;
-      break-inside: avoid-column;
+      display: flex;
+      align-items: center;
+      flex-wrap: wrap;
+      width: 70%;
+      column-gap: 50px;
     `,
   })}
 `
