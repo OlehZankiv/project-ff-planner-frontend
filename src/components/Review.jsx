826bbbba9e67f42ae6336510de3f0d3298175c28
Oleh Zankiv <support@devage.tech>
2023-06-12 12:34:46 +0300

    fixed review issue and added additional expanded logic

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -3,6 +3,7 @@ import { getMobileStyles, ScreenWrapper } from '../styles/breakpoints'
 import { Text } from './Text'
 import { PencilIcon, RatingStarUserIcon, TrashIcon } from '../assets/icons'
 import { Avatar, OpacityButton } from '../components'
+import { useState } from 'react'
 
 export const Review = ({
   style,
@@ -13,26 +14,33 @@ export const Review = ({
   editOnClick,
   deleteOnClick,
 }) => {
+  const [isFullView, setFullView] = useState(false)
+
   const { colors } = useTheme()
 
   const commentJSX = (
-    <DescriptionWrapper>
+    <DescriptionWrapper isFullView={isFullView}>
       <Text type='p'>{comment}</Text>
     </DescriptionWrapper>
   )
 
+  const withStopPropagation = (callback) => (e) => {
+    e.stopPropagation()
+    callback()
+  }
+
   return (
-    <Wrapper style={style}>
+    <Wrapper style={style} onClick={() => setFullView((prev) => !prev)}>
       <Avatar size={50} name={name} image={avatarURL} />
       <InfoWrapper>
         <ReviewTop>
           <Text type='h5'>{name}</Text>
           {showEdit && (
             <EditWrapper>
-              <OpacityButton onClick={editOnClick}>
+              <OpacityButton onClick={withStopPropagation(editOnClick)}>
                 <PencilIcon color={colors.text} />
               </OpacityButton>
-              <OpacityButton onClick={deleteOnClick}>
+              <OpacityButton onClick={withStopPropagation(deleteOnClick)}>
                 <TrashIcon color={colors.text} />
               </OpacityButton>
             </EditWrapper>
@@ -54,7 +62,7 @@ export const Review = ({
   )
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled(OpacityButton)`
   ${({ theme: { colors } }) => css`
     padding: 32px;
     display: flex;
@@ -62,7 +70,6 @@ const Wrapper = styled.div`
     justify-content: flex-start;
     column-gap: 14px;
     border-radius: 8px;
-    overflow: hidden;
     background-color: transparent;
     border: 1px solid ${colors.reviewBorder};
     width: 100%;
@@ -74,14 +81,21 @@ const Wrapper = styled.div`
 
 const InfoWrapper = styled.div`
   flex: 1;
+  max-width: calc(100% - 64px);
 `
 
 const DescriptionWrapper = styled.div`
-  margin-top: 24px;
-  display: -webkit-box;
-  -webkit-line-clamp: 4;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
+  ${({ isFullView }) => css`
+    margin-top: 24px;
+    word-break: break-word;
+    ${!isFullView &&
+    css`
+      display: -webkit-box;
+      -webkit-line-clamp: 4;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+    `}
+  `}
 `
 
 const StarsWrapper = styled.div`
