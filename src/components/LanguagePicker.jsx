1f0c12af897104344d6959cf6e8235549e8f3d54
Oleh Zankiv <support@devage.tech>
2023-06-13 14:18:49 +0300

    Code refactor

--- src/components/LanguagePicker.jsx
+++ src/components/LanguagePicker.jsx
@@ -5,24 +5,25 @@ import { useUpdateUser } from '../hooks/query'
 import { OpacityButton } from './buttons/OpacityButton'
 import { Text } from './Text'
 
-export const LanguagePicker = ({ style, color, fontSize, isUserLogged = false }) => {
+export const LanguagePicker = ({ style, color, fontSize, isUserLogged }) => {
   const { i18n } = useTranslation()
-  const currentLanguage = i18n.language
   const { updateLanguage } = useUpdateUser()
   const { logger } = useAuthContext()
 
+  const currentLanguage = i18n.language
+
   useEffect(() => {
-    if (logger && logger.language) {
-      i18n.changeLanguage(logger.language)
-    }
-  }, [logger])
+    if (logger?.language) i18n.changeLanguage(logger.language)
+  }, [logger?.language])
 
   const handleLanguageChange = () => {
     const newLanguage = currentLanguage === 'en' ? 'uk' : 'en'
+
     i18n.changeLanguage(newLanguage)
-    isUserLogged ?? updateLanguage(newLanguage)
+    isUserLogged && updateLanguage(newLanguage)
     localStorage.setItem('language', newLanguage)
   }
+
   return (
     <OpacityButton onClick={handleLanguageChange} style={style}>
       <Text type='p' color={color} fontWeight={700} fontSize={fontSize}>
