36bf5e7c93198e2a568f6d19acf2c7e564a4aef6
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2023-06-13 12:49:47 +0300

    Update LanguagePicker.jsx

--- src/components/LanguagePicker.jsx
+++ src/components/LanguagePicker.jsx
@@ -1,4 +1,6 @@
 import { useTranslation } from 'react-i18next'
+import { useEffect } from 'react'
+import { useAuthContext } from '../contexts/auth'
 import { useUpdateUser } from '../hooks/query'
 import { OpacityButton } from './buttons/OpacityButton'
 import { Text } from './Text'
@@ -7,11 +9,18 @@ export const LanguagePicker = ({ style, color, fontSize, isUserLogged = false })
   const { i18n } = useTranslation()
   const currentLanguage = i18n.language
   const { updateLanguage } = useUpdateUser()
+  const { logger } = useAuthContext()
+
+  useEffect(() => {
+    if (logger && logger.language) {
+      i18n.changeLanguage(logger.language)
+    }
+  }, [logger])
 
   const handleLanguageChange = () => {
     const newLanguage = currentLanguage === 'en' ? 'uk' : 'en'
     i18n.changeLanguage(newLanguage)
-    isUserLogged && updateLanguage(newLanguage)
+    isUserLogged ?? updateLanguage(newLanguage)
     localStorage.setItem('language', newLanguage)
   }
   return (
