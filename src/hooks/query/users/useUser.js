ad2330d422e46b8c58760f210590a7ddcc53ddf8
Oleh Zankiv <support@devage.tech>
2023-06-15 19:31:47 +0300

    fixed bug

--- src/hooks/query/users/useUser.js
+++ src/hooks/query/users/useUser.js
@@ -3,19 +3,16 @@ import { queryKeys } from '../queryKeys'
 import { getUser } from '../../../api/'
 import { toUser } from '../mappers'
 import { handleRequestError } from '../../../utils/notifications'
-import { useAuthContext } from '../../../contexts/auth'
 
 export const useUser = (id = '') => {
-  const { token } = useAuthContext()
-
   const {
     isLoading,
     isFetching,
     data: user = null,
-  } = useQuery([queryKeys.getUsers, id, token], () => getUser(id), {
+  } = useQuery([queryKeys.getUsers, id], () => getUser(id), {
     onError: handleRequestError,
     select: (res) => toUser(res.data.user),
-    enabled: !!id && !!token,
+    enabled: !!id,
   })
 
   return {
